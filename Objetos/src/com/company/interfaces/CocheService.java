00a9954ae4bf27a686bbdc3bf527d308c4f21cba
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2023-01-17 20:02:10 -0300

    interfaces

---
+++ Objetos/src/com/company/interfaces/CocheService.java
@@ -0,0 +1,8 @@
+package com.company.interfaces;
+
+import com.company.Coche;
+
+public interface CocheService {
+    public Coche crearCocheDemo();
+
+}
---
+++ Objetos/src/com/company/interfaces/CocheServiceSportImpl.java
@@ -0,0 +1,12 @@
+package com.company.interfaces;
+
+import com.company.Coche;
+import com.company.CocheHibrido;
+import com.company.interfaces.CocheService;
+
+public class CocheServiceSportImpl implements CocheService {
+    @Override
+    public Coche crearCocheDemo() {
+        return new CocheHibrido();
+    }
+}
